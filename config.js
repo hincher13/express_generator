6e8e82db6e0fbab123b08188da9b7a12931b7217
Raymond Hinch <rhinch@bluegracegroup.com>
2022-08-25 15:36:46 -0400

    O Auth and User Authentication

--- config.js
+++ config.js
@@ -1,4 +1,8 @@
 module.exports = {
     'secretKey': '12345-67890-09876-54321',
-    'mongoUrl': 'mongodb://localhost:27017/nucampsite'
+    'mongoUrl': 'mongodb://localhost:27017/nucampsite',
+    'facebook': {
+        clientId:'5487095304684746',
+        clientSecret:'0cd67b6025b4e9a3a6b4c79abe0400f4'
+    }
 }
